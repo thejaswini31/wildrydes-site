21b6dc12173d5189c56a90e9f546b4d91b00166c
thejaswini31 <94298694+thejaswini31@users.noreply.github.com>
2025-04-06 13:14:27 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_EnkTrXbuR', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6obte3fc26ern1rpkjqon1o8ui', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '2rr42vduq7vpbdkkgufe65bv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
